83e98218e415f230a80c298857c62bcabbe02e85
fagja <hey.shunya19@gmail.com>
2022-06-19 14:21:13 +0900

    リファクタリング

--- omikuji.py
+++ omikuji.py
@@ -2,8 +2,9 @@ import random
 
 omikuji = ["大吉", "吉", "凶", "中吉 ", "末吉"]
 
-index = random.randint(0, len(omikuji) - 1)
+# index = random.randint(0, len(omikuji) - 1)
+# result = omikuji[index]
 
-result = omikuji[index]
+result = random.choice(omikuji)
 
 print(f"今日の運勢は...{result}")
